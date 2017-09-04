2f2ef42630d8aaf58a659ac793e18fc5b87fa56d
Kristoffer Tonning <tonning@gmail.com>
2017-09-04 15:11:07 -0400

    Initial commit

---
+++ src/Flashable.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Tonning\Flashable;
+
+use ReflectionClass;
+
+trait Flashable
+{
+    public static function bootFlashable()
+    {
+        static::created(function ($model) {
+            if (property_exists($model, 'flashable') && isset($model->flashable['created'])) {
+                return flash($model->flashable['created']);
+            }
+
+            $name = self::getName($model);
+
+            flash("{$name} has been created.");
+        });
+
+        static::updated(function ($model) {
+            if (property_exists($model, 'flashable') && isset($model->flashable['updated'])) {
+                return flash($model->flashable['updated']);
+            }
+
+            $name = self::getName($model);
+
+            flash("{$name} has been updated.");
+        });
+
+        static::deleted(function ($model) {
+            if (property_exists($model, 'flashable') && isset($model->flashable['deleted'])) {
+                return flash($model->flashable['deleted']);
+            }
+
+            $name = self::getName($model);
+
+            flash("{$name} has been deleted.");
+        });
+    }
+
+    protected static function getName($model)
+    {
+        if (method_exists($model, 'getModelName')) {
+            return $model->getModelName();
+        }
+
+        $nameAsArray = preg_split('/(?<=[a-z])(?=[A-Z])/x', (new ReflectionClass($model))->getShortName());
+
+        return join($nameAsArray, " ");
+    }
+}
